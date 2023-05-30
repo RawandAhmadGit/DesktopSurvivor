ae81cf3c98fb929742b08ffc08ff438e8f85bde9
Oliver/SquidSushi_ <35111378+SquidSushi@users.noreply.github.com>
2023-05-30 10:36:54 +0200

    Added a timer to the top left corner

---
+++ Assets/UITimer.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UITimer : MonoBehaviour
+{
+    public TextMeshPro theText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        theText.text = (Mathf.FloorToInt(Time.time) / 60).ToString("D2") + ":" + (Mathf.FloorToInt(Time.time)% 60).ToString("D2");
+    }
+}
